01135cfcb9f609ab3bb429f74442a240010fa199
PrachiSharma98 <99522178+PrachiSharma98@users.noreply.github.com>
2022-06-11 21:42:18 +0530

    Create DeleteNodeInALinkedList.java

---
+++ DeleteNodeInALinkedList.java
@@ -0,0 +1,21 @@
+Input: head = [4,5,1,9], node = 1
+Output: [4,5,9]
+Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling your function.
+  
+  
+  
+  /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val=node.next.val;
+        node.next=node.next.next;
+        
+    }
+}
